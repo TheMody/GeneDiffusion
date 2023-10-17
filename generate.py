2aad156eea0551d727666d5a395fe14e6af0e0fb
Philip Kenneweg <pkenneweg@login-1.GPU.CIT-EC.NET>
2023-10-17 14:45:02 +0200

    zeug

--- generate.py
+++ generate.py
@@ -9,7 +9,7 @@ def generate_sample(model,num_samples = 10000, save = True, savefolder = save_pa
    for i in range(num_samples//config["batch_size"]):
       xt = torch.randn_like(torch.zeros(config["batch_size"],num_channels,gene_size)).to(device)
       label = torch.randint(num_classes, (config["batch_size"],), dtype=torch.int64).to(device)
-      sample = diffusion.sample_from_reverse_process(model,xt, timesteps=10,y= label, guidance = "c_free", w = 0.1)
+      sample = diffusion.sample_from_reverse_process(model,xt, timesteps=max_steps-1,y= label, guidance = "c_free", w = 0.1)
       if save:
          for a in range(config["batch_size"]):
             torch.save((sample[a].cpu().detach(),  label[a].cpu().detach()), savefolder +"/sample"+str(i*config["batch_size"] + a)+".pt")
