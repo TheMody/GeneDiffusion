4036e0e23bf08cb2ff942efdf79e2e1b6cf6d678
Philip <p.kenneweg@unitybox.de>
2024-06-12 11:37:37 +0200

    zeug

--- model.py
+++ model.py
@@ -257,17 +257,17 @@ class PositionalEncoding(nn.Module):
 
 class EncoderModelDiffusion(nn.Module):
     #input should be (batchsize, num_pcas, dim_pcas)
-    def __init__(self, num_classes=2, input_dim = 8,  hidden_dim = 384,n_layers = 12):
+    def __init__(self, num_classes=2, input_dim = 8,  hidden_dim = 384,n_layers = 12, num_genes = 18432):
         super().__init__()
         self.num_classes = num_classes
-        self.EmbeddingLayer = MultichannelLinear(18432, input_dim, hidden_dim,16)
+        self.EmbeddingLayer = MultichannelLinear(num_genes, input_dim, hidden_dim,16)
         self.module_list = nn.ModuleList([nn.TransformerEncoderLayer(d_model=hidden_dim, nhead=6,dim_feedforward=4*hidden_dim, batch_first=True, activation='gelu') for i in range(n_layers)])
         self.classification_token = nn.Parameter(torch.Tensor(hidden_dim), requires_grad=True)
         nn.init.uniform_(self.classification_token, a=-1/math.sqrt(hidden_dim), b=1/math.sqrt(hidden_dim))
         self.time_embedding = nn.Linear(1, hidden_dim)
         self.class_embedding = nn.Embedding(num_classes, hidden_dim)
         self.c_emb_linear = nn.Linear(hidden_dim, hidden_dim)
-        self.DeEmbeddingLayer = MultichannelLinear(18432//16, hidden_dim, input_dim,16, up = True)
+        self.DeEmbeddingLayer = MultichannelLinear(num_genes//16, hidden_dim, input_dim,16, up = True)
         self.dense = nn.Linear(hidden_dim, num_classes)
 
     def forward(self, x, t,y,last_hidden = True, embedding = False):
